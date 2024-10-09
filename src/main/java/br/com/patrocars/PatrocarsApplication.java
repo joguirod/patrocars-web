ccbdaeb04bf7f65b6f63025d98ee32c4739233eb
joguirod <joguicane@gmail.com>
2024-10-09 20:18:08 -0300

    feat: initialize project

---
+++ src/main/java/br/com/patrocars/PatrocarsApplication.java
@@ -0,0 +1,13 @@
+package br.com.patrocars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatrocarsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatrocarsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/patrocars/PatrocarsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.patrocars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatrocarsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
